6cc8eec8278517bd69d74418544d3ed1a6b58221
Duy Huynh <huquduy@gmail.com>
2024-05-15 17:25:34 +0700

    feat: Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BrowserView, MobileView} from 'react-device-detect';
 import Web from './Web'
 import Mobile from './Mobile'
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return <>
     <BrowserView>
+      <CssBaseline />
         <Web/>
     </BrowserView>
     <MobileView>
